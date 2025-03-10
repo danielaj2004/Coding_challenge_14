d37db48ada4d518b7288e7808ae4c003897c911d
danielaj2004 <danielajoya12@gmail.com>
2025-03-09 20:18:24 -0400

    Task 5 - Inline Editing for Support Tickets

--- cc_14.js
+++ cc_14.js
@@ -30,3 +30,43 @@ function addTicket(customerName, issueDescription, priorityLevel) {
     ticket.appendChild(priorityState);
     ticket.appendChild(resolveButton);
     ticketContainer.appendChild(ticket); // appending ticket to container
+
+     // task 5 Additional Challenge – Inline Editing of Support Tickets
+     ticket.addEventListener('dblclick', function() {
+        const nameInput = document.createElement('input');
+        nameInput.setAttribute('type', 'text');
+        nameInput.value = nameHeading.textContent;
+
+        const issueInput = document.createElement('input');
+        issueInput.setAttribute('type', 'text');
+        issueInput.value = issueText.textContent;
+
+        const priorityInput = document.createElement('input');
+        priorityInput.setAttribute('type', 'text');
+        priorityInput.value = priorityState.textContent.split(": ")[1]; // adding current priority
+
+        const saveButton = document.createElement('button');
+        saveButton.textContent = 'Save'; // creating save button
+
+        ticket.innerHTML = ''; // clearing current content
+
+        ticket.appendChild(nameInput);
+        ticket.appendChild(issueInput);
+        ticket.appendChild(priorityInput);
+        ticket.appendChild(saveButton); // adding appending 
+
+        saveButton.addEventListener('click', function() { // save button listener
+
+            nameHeading.textContent = nameInput.value;
+            issueText.textContent = issueInput.value;
+            priorityState.textContent = `Priority: ${priorityInput.value}`;
+            priorityState.setAttribute('class', priorityInput.value.toLowerCase()); // updating new inputs
+
+            ticket.innerHTML = '';
+            ticket.appendChild(nameHeading);
+            ticket.appendChild(issueText);
+            ticket.appendChild(priorityState);
+            ticket.appendChild(resolveButton);
+        }); // moving back fields to static text
+    });
+}
